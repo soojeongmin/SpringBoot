361a632e8cd52bc1167206ad155f4a1efa70ca37
soojeongmin <soojeongmin@soongsil.ac.kr>
2024-08-14 10:02:52 +0900

    0814

--- _01_Spring_API/src/main/java/com/bit/springboard/controller/ApiController.java
+++ _01_Spring_API/src/main/java/com/bit/springboard/controller/ApiController.java
@@ -1,12 +1,16 @@
 package com.bit.springboard.controller;
 
 import com.bit.springboard.dto.MemberDto;
+import com.bit.springboard.dto.ResponseDto;
 import com.bit.springboard.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 // @RestController = @Controller + @ResponseBody
 @RestController
@@ -17,9 +21,17 @@ public class ApiController {
 
     @PostMapping("/members")
     public ResponseEntity<?> save(MemberDto memberDto) {
-        MemberDto savedMemberDto = memberService.save(memberDto);
-        return ResponseEntity.ok(savedMemberDto);
+        ResponseDto<MemberDto> responseDto = new ResponseDto<>();
+        try {
+            MemberDto savedMemberDto = memberService.save(memberDto);
+            responseDto.setStatusCode(HttpStatus.CREATED.value());
+            responseDto.setStatusMessage("CREATED");
+            responseDto.setData(savedMemberDto);
+            return ResponseEntity.ok(responseDto);
+        } catch (Exception e) {
+            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            responseDto.setStatusMessage(e.getMessage());
+            return ResponseEntity.internalServerError().body(responseDto);
+        }
     }
-
-
 }

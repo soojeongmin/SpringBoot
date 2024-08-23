e7188c01fb48e2e597c3bd8846f9acae7a2400e8
soojeongmin <soojeongmin@soongsil.ac.kr>
2024-08-23 15:25:09 +0900

    0823_e

---
+++ _03_Spring_JPA/src/main/java/com/bit/springboard/repository/FreeBoardRepository.java
@@ -0,0 +1,8 @@
+package com.bit.springboard.repository;
+
+import com.bit.springboard.entity.FreeBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
+    
+}
